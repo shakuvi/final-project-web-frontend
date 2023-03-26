ac37c1b36b95cac16bf242d014c263a33d59ab17
shakuvi <shakuviakash201@gmail.com>
2023-03-26 13:34:47 +0530

    Review table created

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import OrderDashboard from "./pages/OrderDashboard";
 import UserDashBoard from "./pages/UserDashBoard";
 import ReviewsDashboard from "./pages/ReviewsDashboard";
 import FoodManageDashbaord from "./pages/FoodManageDashbaord";
-import PaymentDashboard from "./pages/PaymentDashboard";
+// import PaymentDashboard from "./pages/PaymentDashboard";
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
           <Route path="/users" element={<UserDashBoard />} />
           <Route path="/reviews" element={<ReviewsDashboard />} />
           <Route path="/foods" element={<FoodManageDashbaord />} />
-          <Route path="/payment" element={<PaymentDashboard />} />
+          {/* <Route path="/payment" element={<PaymentDashboard />} /> */}
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
--- src/layouts/Review/ReviewLayout.js
+++ src/layouts/Review/ReviewLayout.js
@@ -1,10 +1,14 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import Review from "./Review";
 
 export default function ReviewLayout() {
   return (
     <div style={{ textAlign: "left" }}>
       <Typography>Reviews</Typography>
+      <div style={{ paddingTop: 15, paddingRight: 20 }}>
+        <Review />
+      </div>
     </div>
   );
 }
